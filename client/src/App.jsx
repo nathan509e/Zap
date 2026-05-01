0413678c1df71c59d5271f1261326925edc06e2b
Nathan Maximiano <31361843+nathan509e@users.noreply.github.com>
2026-05-01 15:45:18 -0300

    1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import MessageComposer from './components/MessageComposer'
 import SendProgress from './components/SendProgress'
 import QRCode from 'qrcode'
 
-const API_URL = 'http://187.127.251.137:3001'
+const API_URL = 'https://commit-abu-electricity-riding.trycloudflare.com'
 
 function App() {
   const [contacts, setContacts] = useState(() => {
